522e9f6e92c72b4c6b1ce2e5b21cf8214d56934a
fabiolorenzi <fabiolorenzi13975@gmail.com>
2022-02-16 21:31:33 +0000

    developed script to follow the camera

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    private Transform player;
+    private Vector3 tempPos;
+
+    [SerializeField]
+    private float minX, maxX;
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+    }
+    void LateUpdate()
+    {
+        if (!player)
+        {
+            return;
+        }
+
+        tempPos = transform.position;
+        tempPos.x = player.position.x;
+
+        minX = 2.5f;
+        maxX = 100f;
+
+        if (tempPos.x < minX)
+        {
+            tempPos.x = minX;
+        }
+        
+        if (tempPos.x > maxX)
+        {
+            tempPos.x = maxX;
+        }
+
+        transform.position = tempPos;
+    }
+}
