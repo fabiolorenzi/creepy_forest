16f276ea565b60c966c15edd57ca7f39fc1ef62f
fabiolorenzi <fabiolorenzi13975@gmail.com>
2022-02-19 23:45:54 +0000

    solve rotation bug

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -4,6 +4,25 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    private int rotationY;
+
+    private void Start()
+    {
+        rotationY = 0;
+        StartCoroutine(Rotator());
+    }
+
+    IEnumerator Rotator()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+
+            rotationY += 1;
+            transform.Rotate(0, rotationY, 0);
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
