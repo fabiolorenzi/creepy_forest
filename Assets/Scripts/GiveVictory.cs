01ce14f1223635087638be1c5b90a2b37ab47a70
fabiolorenzi <fabiolorenzi13975@gmail.com>
2022-03-04 19:22:56 +0000

    developed code to give victory based on points

---
+++ Assets/Scripts/GiveVictory.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GiveVictory : MonoBehaviour
+{
+    public GameObject HomeButton;
+    public GameObject NextLevelButton;
+
+    private static LevelsReq[] levels = {
+        new LevelsReq("level_one", 3)
+    };
+
+    private string currentLevel;
+    private int levelsPoints;
+
+    public void Awake()
+    {
+        currentLevel = SceneManager.GetActiveScene().name;
+        for (int x = 0; x < levels.Length; x++)
+        {
+            if (currentLevel == levels[x].Name)
+            {
+                levelsPoints = levels[x].Points;
+            }
+        }
+    }
+
+    public void Update()
+    {
+        if (Player.score == levelsPoints)
+        {
+            HomeButton.SetActive(true);
+            NextLevelButton.SetActive(true);
+        }
+    }
+}
